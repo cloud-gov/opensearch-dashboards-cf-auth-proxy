a2ff09b965bc2b24c74b3ed89007dab638ceb444
Ben Berry <ben.berry@gsa.gov>
2021-01-19 15:15:21 -0800

    add basic proxy function and tests

---
+++ kibana_cf_auth_proxy/proxy.py
@@ -0,0 +1,20 @@
+from flask import Response
+
+import requests
+
+def proxy_request(url, headers, data, cookies, method):
+    resp = requests.request(
+        method=method,
+        url=url,
+        headers=headers,
+        data=data,
+        cookies=cookies,
+        allow_redirects=False
+    )
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
