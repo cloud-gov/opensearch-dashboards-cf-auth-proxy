94ccbcc982519538b96d699f3e072d8aef5e6053
Ben Berry <ben.berry@gsa.gov>
2021-02-11 15:28:55 -0800

    fix variable syntax, fix directory locating
    plz work. plz.

--- ci/update-networking.sh
+++ ci/update-networking.sh
@@ -3,7 +3,13 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd ${dir}
+trap popd exit
+
 cf api ${CF_API_URL}
 cf auth 
 cf t -o ${CF_ORGANIZATION} -s ${CF_SPACE}
-./dev cf-network
+
+../dev cf-network
