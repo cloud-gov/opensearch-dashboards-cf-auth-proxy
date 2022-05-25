f67928e739c4503301e001a0b566f72154937c3b
Mark Boyd <mark.boyd@gsa.gov>
2022-05-25 11:36:39 -0400

    update to python 3.9

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 apt-get update
-apt-get install -y python3.8 python3-venv
+apt-get install -y python3.9 python3-venv
 
 src/dev set-up-ci-environment
 src/dev e2e
