84ba70ec071b65f1b0cc2f65416dd77e5d583c8e
Mark Boyd <mark.boyd@gsa.gov>
2022-05-26 17:24:20 -0400

    update URL handling

--- kibana_cf_auth_proxy/cf.py
+++ kibana_cf_auth_proxy/cf.py
@@ -1,5 +1,7 @@
 import requests
 
+from urllib.parse import urljoin
+
 from kibana_cf_auth_proxy.extensions import config
 
 
@@ -19,7 +21,7 @@ def iterate_cf_resource(session, first_request):
 def get_spaces_for_user(user_id, token):
     with requests.Session() as s:
         s.headers["Authorization"] = f"Bearer {token}"
-        url = f"{config.CF_URL}v3/roles?user_guids={user_id}&types={','.join(config.PERMITTED_SPACE_ROLES)}"
+        url = f"{urljoin(config.CF_URL, 'v3/roles')}?user_guids={user_id}&types={','.join(config.PERMITTED_SPACE_ROLES)}"
         resources = iterate_cf_resource(s, url)
     spaces = [r["relationships"]["space"]["data"]["guid"] for r in resources]
     return spaces
@@ -28,7 +30,7 @@ def get_spaces_for_user(user_id, token):
 def get_orgs_for_user(user_id, token):
     with requests.Session() as s:
         s.headers["Authorization"] = f"Bearer {token}"
-        url = f"{config.CF_URL}v3/roles?user_guids={user_id}&types={','.join(config.PERMITTED_ORG_ROLES)}"
+        url = f"{urljoin(config.CF_URL, 'v3/roles')}?user_guids={user_id}&types={','.join(config.PERMITTED_ORG_ROLES)}"
         resources = iterate_cf_resource(s, url)
     orgs = [r["relationships"]["organization"]["data"]["guid"] for r in resources]
     return orgs
