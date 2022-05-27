6b6b953f6c89c49b89b48bdbbbb1e6e2811f8595
Mark Boyd <mark.boyd@gsa.gov>
2022-05-27 14:05:49 -0400

    rename CF_URL -> CF_API_URL

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -10,7 +10,7 @@ def test_config_loads(monkeypatch):
     assert config.KIBANA_URL == "mock://kibana/"
     assert config.UAA_AUTH_URL == "mock://uaa/authorize"
     assert config.UAA_TOKEN_URL == "mock://uaa/token"
-    assert config.CF_URL == "mock://cf/"
+    assert config.CF_API_URL == "mock://cf/"
     assert config.UAA_CLIENT_ID == "EXAMPLE"
     assert config.UAA_CLIENT_SECRET == "example"
     assert config.SECRET_KEY == "CHANGEME"
@@ -33,7 +33,7 @@ def test_local_config(monkeypatch, kibana_url):
     monkeypatch.setenv("FLASK_ENV", "local")
     monkeypatch.setenv("PORT", "8888")
     monkeypatch.setenv("KIBANA_URL", kibana_url)
-    monkeypatch.setenv("CF_URL", "https://api.example.com/")
+    monkeypatch.setenv("CF_API_URL", "https://api.example.com/")
     monkeypatch.setenv("UAA_AUTH_URL", "https://uaa.example.com/authorize")
     monkeypatch.setenv("UAA_TOKEN_URL", "https://uaa.example.com/token")
     monkeypatch.setenv("UAA_CLIENT_ID", "feedabee")
@@ -49,7 +49,7 @@ def test_local_config(monkeypatch, kibana_url):
     assert config.UAA_TOKEN_URL == "https://uaa.example.com/token"
     assert config.UAA_CLIENT_ID == "feedabee"
     assert config.UAA_CLIENT_SECRET == "CHANGEME"
-    assert config.CF_URL == "https://api.example.com/"
+    assert config.CF_API_URL == "https://api.example.com/"
     assert config.SECRET_KEY == "changeme"
     assert config.PERMANENT_SESSION_LIFETIME == 3600
 
@@ -63,7 +63,7 @@ def test_prod_config(monkeypatch, kibana_url):
     monkeypatch.setenv("KIBANA_URL", kibana_url)
     monkeypatch.setenv("UAA_AUTH_URL", "https://uaa.example.com/authorize")
     monkeypatch.setenv("UAA_TOKEN_URL", "https://uaa.example.com/token")
-    monkeypatch.setenv("CF_URL", "https://api.example.com/")
+    monkeypatch.setenv("CF_API_URL", "https://api.example.com/")
     monkeypatch.setenv("UAA_CLIENT_ID", "feedabee")
     monkeypatch.setenv("UAA_CLIENT_SECRET", "CHANGEME")
     monkeypatch.setenv("SECRET_KEY", "changeme")
@@ -77,7 +77,7 @@ def test_prod_config(monkeypatch, kibana_url):
     assert config.UAA_TOKEN_URL == "https://uaa.example.com/token"
     assert config.UAA_CLIENT_ID == "feedabee"
     assert config.UAA_CLIENT_SECRET == "CHANGEME"
-    assert config.CF_URL == "https://api.example.com/"
+    assert config.CF_API_URL == "https://api.example.com/"
     assert config.SECRET_KEY == "changeme"
     assert config.PERMANENT_SESSION_LIFETIME == 3600
     assert config.PERMITTED_SPACE_ROLES == [
