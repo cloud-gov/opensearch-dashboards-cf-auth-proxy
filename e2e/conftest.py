13f4933a45539d756bbd5550a12be54b5acbfd4e
Mark Boyd <mark.boyd@gsa.gov>
2023-08-30 17:39:55 -0400

    add new user fixtures

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -10,14 +10,10 @@ def user_1():
     user_1_username = getenv(f"TEST_USER_1_USERNAME")
     user_1_password = getenv(f"TEST_USER_1_PASSWORD")
     user_1_totp_seed = getenv(f"TEST_USER_1_TOTP_SEED")
-    user_1_space_id = getenv(f"TEST_USER_1_SPACE_ID")
-    user_1_org_id = getenv(f"TEST_USER_1_ORG_ID")
     user_1 = User(
         user_1_username,
         user_1_password,
         user_1_totp_seed,
-        user_1_space_id,
-        user_1_org_id,
     )
     return user_1
 
@@ -27,13 +23,33 @@ def user_2():
     user_2_username = getenv(f"TEST_USER_2_USERNAME")
     user_2_password = getenv(f"TEST_USER_2_PASSWORD")
     user_2_totp_seed = getenv(f"TEST_USER_2_TOTP_SEED")
-    user_2_space_id = getenv(f"TEST_USER_2_SPACE_ID")
-    user_2_org_id = getenv(f"TEST_USER_2_ORG_ID")
     user_2 = User(
         user_2_username,
         user_2_password,
         user_2_totp_seed,
-        user_2_space_id,
-        user_2_org_id,
     )
     return user_2
+
+@pytest.fixture
+def user_3():
+    user_3_username = getenv(f"TEST_USER_3_USERNAME")
+    user_3_password = getenv(f"TEST_USER_3_PASSWORD")
+    user_3_totp_seed = getenv(f"TEST_USER_3_TOTP_SEED")
+    user_3 = User(
+        user_3_username,
+        user_3_password,
+        user_3_totp_seed,
+    )
+    return user_3
+
+@pytest.fixture
+def user_4():
+    user_4_username = getenv(f"TEST_USER_4_USERNAME")
+    user_4_password = getenv(f"TEST_USER_4_PASSWORD")
+    user_4_totp_seed = getenv(f"TEST_USER_4_TOTP_SEED")
+    user_4 = User(
+        user_4_username,
+        user_4_password,
+        user_4_totp_seed,
+    )
+    return user_4
