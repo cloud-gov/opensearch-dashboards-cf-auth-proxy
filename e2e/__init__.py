65d2ea51b72adf5397951935ac8e44f42f4f08a1
Mark Boyd <mark.boyd@gsa.gov>
2023-11-21 16:40:13 -0500

    refactor tests to avoid arbitrary timeouts and rely on element assertisons

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -1,3 +1,4 @@
 from os import getenv
 
 AUTH_PROXY_URL = getenv("AUTH_PROXY_URL")
+UAA_AUTH_URL = getenv("UAA_AUTH_URL")
--- e2e/utils.py
+++ e2e/utils.py
@@ -1,10 +1,9 @@
 import re
 
-from . import AUTH_PROXY_URL
-
+from . import AUTH_PROXY_URL, UAA_AUTH_URL
 
 def log_in(user, page, start_at=None):
-    page.set_default_timeout(60000)
+    # page.set_default_timeout(30000)
 
     if start_at is None:
         start_at = AUTH_PROXY_URL
@@ -41,20 +40,20 @@ def log_in(user, page, start_at=None):
     login_button.wait_for()
     login_button.click()
 
-    # lots of redirects and stuff happen here, so just, like, chill, ok?
-    page.wait_for_load_state("networkidle")
+    # wait for OAuth authorize page or auth proxy page
+    page.wait_for_url(re.compile(f"({AUTH_PROXY_URL}|{UAA_AUTH_URL})"))
 
     if "/authorize?" in page.url:
         # first time using this app with this user
         authorize_button = page.get_by_text("Authorize")
         authorize_button.wait_for()
         authorize_button.click()
-        page.wait_for_load_state("networkidle")
 
     page.wait_for_url(f"{AUTH_PROXY_URL}*")
 
-    # handle first-login stuff when it's here
-    page.wait_for_timeout(5000)
+    # Ensure that dashboards has loaded
+    home_title = page.get_by_text("Home")
+    home_title.wait_for()
 
     if page.get_by_text("Start by adding your data").is_visible():
         explore_button = page.get_by_text("Explore on my own")
@@ -75,9 +74,15 @@ def switch_tenants(page, tenant="Global"):
     submit_button.wait_for()
     submit_button.click()
 
+    loading_text = page.get_by_text("Loading Opensearch Dashboards")
+    loading_text.wait_for()
+
+    home_title = page.get_by_text("Home")
+    home_title.wait_for()
+
     # todo: there is a page refresh that happens after submitting the tenant option.
     # we should wait on an element instead of arbitrary timeout
-    page.wait_for_timeout(2000)
+    # page.wait_for_timeout(2000)
 
 
 def go_to_discover_page(page):
