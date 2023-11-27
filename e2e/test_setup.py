397474ba43426f6274e3c6e63875b0fbb04126cb
Mark Boyd <mark.boyd@gsa.gov>
2023-11-27 11:37:23 -0500

    remove blank lines

--- e2e/test_setup.py
+++ e2e/test_setup.py
@@ -7,7 +7,6 @@ def test_redirects_to_login(page):
     page.goto(AUTH_PROXY_URL)
     assert "login" in page.url
 
-
 def test_login_redirects_home_without_slash(page, user_1):
     # this tests an actual bug where going to the home page without
     # a trailing slash (e.g. logs.example.com) causes a redirect
@@ -15,12 +14,10 @@ def test_login_redirects_home_without_slash(page, user_1):
     log_in(user_1, page, str.rstrip(AUTH_PROXY_URL, "/"))
     assert page.url.startswith(urljoin(AUTH_PROXY_URL, "app/home"))
 
-
 def test_login_redirects_home(page, user_1):
     # this tests the basic case - going to logs.example.com/
     log_in(user_1, page)
     assert page.url.startswith(urljoin(AUTH_PROXY_URL, "app/home"))
-
 
 def test_login_remembers_target(page, user_1):
     log_in(user_1, page, urljoin(AUTH_PROXY_URL, "app/dev_tools"))
