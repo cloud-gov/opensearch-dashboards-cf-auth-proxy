f92c75a83135009ee0078c5b8e43f0e7479592dd
Mark Boyd <mark.boyd@gsa.gov>
2024-02-15 16:50:29 -0500

    add script to download wheels

---
+++ bin/download-wheels.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export PYTHON_VERSION="3.12"
+
+grep -v "eventlet\|flask" < pip-tools/requirements.in | xargs -I {} \
+  python3 -m pip download {} \
+  --platform linux_x86_64 \
+  --only-binary=:all: \
+  --python-version "$PYTHON_VERSION"
+
+# download flask
+python3 -m pip download flask \
+  --platform manylinux_2_17_x86_64 \
+  --platform linux_x86_64 --only-binary=:all: \
+  --python-version "$PYTHON_VERSION"
+
+# download eventlet
+python3 -m pip download eventlet \
+  --only-binary=:all: \
+  --python-version "$PYTHON_VERSION"
