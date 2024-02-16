7c5e1187c2521da8a982d56dd37c8ded8c52be67
Mark Boyd <mark.boyd@gsa.gov>
2024-02-16 16:01:45 -0500

    fix command to run bandit in CI

--- ci/test.sh
+++ ci/test.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 src/dev set-up-environment
-src/venv/bin/bandit -r opensearch_dashboards_cf_auth_proxy
+src/venv/bin/bandit -r src/src/cf_auth_proxy
 src/dev test
