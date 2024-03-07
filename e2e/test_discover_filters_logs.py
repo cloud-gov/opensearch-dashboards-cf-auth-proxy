c3895f09f1a0ce049187f770a50e20428c6cdd9f
Mark Boyd <mark.boyd@gsa.gov>
2024-03-07 11:35:43 -0500

    update playwright e2e test to use auto-waiting assertions

--- e2e/test_discover_filters_logs.py
+++ e2e/test_discover_filters_logs.py
@@ -1,3 +1,4 @@
+from playwright.sync_api import expect
 from .utils import log_in, switch_tenants, go_to_discover_page
 
 
@@ -8,13 +9,13 @@ def test_see_correct_logs_in_discover_user_1(user_1, page):
 
     go_to_discover_page(page)
 
-    assert page.get_by_text("1 hit").count() == 1
-    assert page.get_by_text("org_id_1").count() == 0
-    assert page.get_by_text("org_id_2").count() == 0
-    assert page.get_by_text("space_id_1").count() == 1
-    assert page.get_by_text("space_id_2").count() == 0
-    assert page.get_by_text("org_1_both_orgs_space").count() == 0
-    assert page.get_by_text("org_2_both_orgs_space").count() == 0
+    expect(page.get_by_text("1 hit")).to_be_visible()
+    expect(page.get_by_text("org_id_1")).not_to_be_visible()
+    expect(page.get_by_text("org_id_2")).not_to_be_visible()
+    expect(page.get_by_text("space_id_1")).to_be_visible()
+    expect(page.get_by_text("space_id_2")).not_to_be_visible()
+    expect(page.get_by_text("org_1_both_orgs_space")).not_to_be_visible()
+    expect(page.get_by_text("org_2_both_orgs_space")).not_to_be_visible()
 
 
 def test_see_correct_logs_in_discover_user_2(user_2, page):
@@ -24,13 +25,13 @@ def test_see_correct_logs_in_discover_user_2(user_2, page):
 
     go_to_discover_page(page)
 
-    assert page.get_by_text("3 hits").count() == 1
-    assert page.get_by_text("org_id_1").count() == 0
-    assert page.get_by_text("org_id_2").count() == 1
-    assert page.get_by_text("space_id_1").count() == 0
-    assert page.get_by_text("space_id_2").count() == 1
-    assert page.get_by_text("org_1_both_orgs_space").count() == 0
-    assert page.get_by_text("org_2_both_orgs_space").count() == 1
+    expect(page.get_by_text("3 hits")).to_be_visible()
+    expect(page.get_by_text("org_id_1")).not_to_be_visible()
+    expect(page.get_by_text("org_id_2")).to_be_visible()
+    expect(page.get_by_text("space_id_1")).not_to_be_visible()
+    expect(page.get_by_text("space_id_2")).to_be_visible()
+    expect(page.get_by_text("org_1_both_orgs_space")).not_to_be_visible()
+    expect(page.get_by_text("org_2_both_orgs_space")).to_be_visible()
 
 
 def test_see_correct_logs_in_discover_user_3(user_3, page):
@@ -40,13 +41,13 @@ def test_see_correct_logs_in_discover_user_3(user_3, page):
 
     go_to_discover_page(page)
 
-    assert page.get_by_text("2 hits").count() == 1
-    assert page.get_by_text("org_id_1").count() == 0
-    assert page.get_by_text("org_id_2").count() == 0
-    assert page.get_by_text("space_id_1").count() == 1
-    assert page.get_by_text("space_id_2").count() == 1
-    assert page.get_by_text("org_1_both_orgs_space").count() == 0
-    assert page.get_by_text("org_2_both_orgs_space").count() == 0
+    expect(page.get_by_text("2 hits")).to_be_visible()
+    expect(page.get_by_text("org_id_1")).not_to_be_visible()
+    expect(page.get_by_text("org_id_2")).not_to_be_visible()
+    expect(page.get_by_text("space_id_1")).to_be_visible()
+    expect(page.get_by_text("space_id_2")).to_be_visible()
+    expect(page.get_by_text("org_1_both_orgs_space")).not_to_be_visible()
+    expect(page.get_by_text("org_2_both_orgs_space")).not_to_be_visible()
 
 
 def test_see_correct_logs_in_discover_user_4(user_4, page):
@@ -56,10 +57,10 @@ def test_see_correct_logs_in_discover_user_4(user_4, page):
 
     go_to_discover_page(page)
 
-    assert page.get_by_text("1 hit").count() == 1
-    assert page.get_by_text("org_id_1").count() == 0
-    assert page.get_by_text("org_id_2").count() == 0
-    assert page.get_by_text("space_id_1").count() == 0
-    assert page.get_by_text("space_id_2").count() == 0
-    assert page.get_by_text("org_1_both_orgs_space").count() == 1
-    assert page.get_by_text("org_2_both_orgs_space").count() == 0
+    expect(page.get_by_text("1 hit")).to_be_visible()
+    expect(page.get_by_text("org_id_1")).not_to_be_visible()
+    expect(page.get_by_text("org_id_2")).not_to_be_visible()
+    expect(page.get_by_text("space_id_1")).not_to_be_visible()
+    expect(page.get_by_text("space_id_2")).not_to_be_visible()
+    expect(page.get_by_text("org_1_both_orgs_space")).to_be_visible()
+    expect(page.get_by_text("org_2_both_orgs_space")).not_to_be_visible()
--- e2e/test_setup.py
+++ e2e/test_setup.py
@@ -1,3 +1,5 @@
+import re
+from playwright.sync_api import expect
 from urllib.parse import urljoin
 from . import AUTH_PROXY_URL
 from .utils import log_in
@@ -5,7 +7,7 @@ from .utils import log_in
 
 def test_redirects_to_login(page):
     page.goto(AUTH_PROXY_URL)
-    assert "login" in page.url
+    expect(page).to_have_url(re.compile(".*login.*"))
 
 
 def test_login_redirects_home_without_slash(page, user_1):
@@ -13,15 +15,17 @@ def test_login_redirects_home_without_slash(page, user_1):
     # a trailing slash (e.g. logs.example.com) causes a redirect
     # to an invalid endpoint.
     log_in(user_1, page, str.rstrip(AUTH_PROXY_URL, "/"))
-    assert page.url.startswith(urljoin(AUTH_PROXY_URL, "app/home"))
+    expect(page).to_have_url(re.compile(f"{urljoin(AUTH_PROXY_URL, 'app/home')}.*"))
 
 
 def test_login_redirects_home(page, user_1):
     # this tests the basic case - going to logs.example.com/
     log_in(user_1, page)
-    assert page.url.startswith(urljoin(AUTH_PROXY_URL, "app/home"))
+    expect(page).to_have_url(re.compile(f"{urljoin(AUTH_PROXY_URL, 'app/home')}.*"))
 
 
 def test_login_remembers_target(page, user_1):
     log_in(user_1, page, urljoin(AUTH_PROXY_URL, "app/dev_tools"))
-    assert page.url.startswith(urljoin(AUTH_PROXY_URL, "app/dev_tools"))
+    expect(page).to_have_url(
+        re.compile(f"{urljoin(AUTH_PROXY_URL, 'app/dev_tools')}.*")
+    )
