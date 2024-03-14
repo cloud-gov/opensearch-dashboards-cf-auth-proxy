329cf988fcec2f87aa10f3a5b02e4b3dda164f8c
= <ben.berry@gsa.gov>
2024-03-14 16:04:21 -0700

    revert timeout, add debug, allow setting ci target

--- scripts/download-e2e-ci-results.sh
+++ scripts/download-e2e-ci-results.sh
@@ -7,7 +7,7 @@ if [[ -z "$BUILD_NUMBER" ]]; then
   exit 1
 fi
 
-CI_TASK_TARGET="fly -t ci intercept -j opensearch-dashboards-cf-auth-proxy/e2e -s e2e-tests -b $BUILD_NUMBER"
+CI_TASK_TARGET="fly -t ${FLY_TARGET:=ci} intercept -j opensearch-dashboards-cf-auth-proxy/e2e -s e2e-tests -b $BUILD_NUMBER"
 TEST_RESULTS_DIR="src/test-results"
 LOCAL_TARGET_DIR="ci-test-results"
 
