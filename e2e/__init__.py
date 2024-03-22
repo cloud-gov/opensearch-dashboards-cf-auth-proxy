a89b12cfe87e0a8bd0410b0de781c5a9f547fa68
Mark Boyd <mark.boyd@gsa.gov>
2024-03-22 16:46:58 -0400

    add basic env var validation for e2e tests

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -1,4 +1,12 @@
 from os import getenv
+import sys
 
 AUTH_PROXY_URL = getenv("AUTH_PROXY_URL")
+if AUTH_PROXY_URL is None:
+    print("AUTH_PROXY_URL is a required environment variable, exiting")
+    sys.exit(1)
+
 UAA_AUTH_URL = getenv("UAA_AUTH_URL")
+if UAA_AUTH_URL is None:
+    print("UAA_AUTH_URL is a required environment variable, exiting")
+    sys.exit(1)
