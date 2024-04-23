a6fe01e77ceced476dbd4bf89d9e8dd18ed96eeb
Jason A. Gambino <jason.gambino@gsa.gov>
2024-04-23 13:36:01 -0400

    hard code for test

--- cf_auth_proxy/app.py
+++ cf_auth_proxy/app.py
@@ -178,7 +178,7 @@ def create_app():
                 + ","
                 + str(session.get("orgs", []))
             ]
-            headers["x-proxy-roles"] = list_to_ext_header(roles)
+            headers["x-proxy-roles"] = ["user", "sandbox-gsa"]
 
         # TODO: add x-forwarded-for functionality
         headers["x-forwarded-for"] = "127.0.0.1"
