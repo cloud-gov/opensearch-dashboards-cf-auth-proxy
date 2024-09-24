6d04fb105b55e962499a3fce018430fc610cce2f
Mark Boyd <mark.boyd@gsa.gov>
2024-09-24 16:19:19 -0400

    update script logic and variable references

--- ci/update-test-user-passwords.sh
+++ ci/update-test-user-passwords.sh
@@ -4,14 +4,18 @@
 uaac target "$UAA_API_URL"
 uaac token client get "$UAA_CLIENT_ID" -s "$UAA_CLIENT_SECRET"
 
-TEST_USER_CREDENTIAL_NAMES=$(echo "$CREDENTIAL_USERNAME_MAP" | jq '. | keys | join(" ")')
+TEST_USER_CREDENTIAL_NAMES=$(echo "$TEST_USERS_CREDENTIAL_USERNAME_MAP" | jq '. | keys | join(" ")')
 
 for credential_name in $TEST_USER_CREDENTIAL_NAMES; do
+  echo "updating password credential for $credential_name"
+
   # Generate a new password for the credential
   credhub regenerate -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name"
 
   # Get the UAA username for the corresponding Credhub credential
   USERNAME=$(echo "$CREDENTIAL_USERNAME_MAP" | jq --arg credential_name "$credential_name" '.[$credential_name]')
+
+  echo "updating UAA password for $USERNAME"
 
   # Get the new password from Credhub
   PASSWORD=$(credhub get -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name" --output-json | jq -r '.value')
