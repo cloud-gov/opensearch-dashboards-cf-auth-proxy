907f2f73ec0a1a69eaec60786d68ba518d9d776b
Mark Boyd <mark.boyd@gsa.gov>
2024-09-24 16:04:24 -0400

    add script to update test user passwords

---
+++ ci/update-test-user-passwords.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Login to UAA
+uaac target "$UAA_API_URL"
+uaac token client get "$UAA_CLIENT_ID" -s "$UAA_CLIENT_SECRET"
+
+TEST_USER_CREDENTIAL_NAMES=$(echo "$CREDENTIAL_USERNAME_MAP" | jq '. | keys | join(" ")')
+
+for credential_name in $TEST_USER_CREDENTIAL_NAMES; do
+  # Generate a new password for the credential
+  credhub regenerate -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name"
+
+  # Get the UAA username for the corresponding Credhub credential
+  USERNAME=$(echo "$CREDENTIAL_USERNAME_MAP" | jq --arg credential_name "$credential_name" '.[$credential_name]')
+
+  # Get the new password from Credhub
+  PASSWORD=$(credhub get -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name" --output-json | jq -r '.value')
+
+  # Update the user password in UAA with the new value from Credhub
+  uaac password set "$USERNAME" --password "$PASSWORD"
+
+  # Activate the user, just to be safe
+  uaac user activate "$USERNAME"
+done
+
+
+
