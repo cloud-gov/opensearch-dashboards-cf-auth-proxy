abe52033dd5af60cb4a74355818234934f3a9a36
Mark Boyd <mark.boyd@gsa.gov>
2024-10-30 16:33:33 -0400

    add config for redis sessions

--- cf_auth_proxy/config.py
+++ cf_auth_proxy/config.py
@@ -1,5 +1,5 @@
 import json
-
+from redis import Redis
 
 from environs import Env
 from jwcrypto import jwk
@@ -85,7 +85,9 @@ class AppConfig(Config):
         self.DASHBOARD_URL = self.env_parser.str("DASHBOARD_URL")
         if self.DASHBOARD_URL[-1] != "/":
             self.DASHBOARD_URL = f"{self.DASHBOARD_URL}/"
-        self.SESSION_TYPE = "null"
+
+        self.SESSION_TYPE = "redis"
+        self.SESSION_REDIS = Redis(host=self.env_parser.str("REDIS_HOST"), port=6379)
         self.SESSION_COOKIE_SECURE = True
         self.PERMANENT_SESSION_LIFETIME = self.env_parser.int("SESSION_LIFETIME")
 
@@ -115,5 +117,5 @@ class LocalConfig(AppConfig):
         super().__init__()
         self.TESTING = True
         self.DEBUG = True
-        self.SESSION_TYPE = "filesystem"
+        # self.SESSION_TYPE = "filesystem"
         self.SESSION_COOKIE_SECURE = False
